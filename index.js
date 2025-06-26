b61c7475bed6b0d9bf2a5328a899a76283d57d36
Yasodha456 <yasodhamercy27@gmail.com>
2025-06-26 20:09:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/properties", propertyRoutes);
-
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
